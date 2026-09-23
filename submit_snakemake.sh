ffe47433503831d4ca7daaa189c9d3cf1fe628c1
lemkhoi <97572491+lemkhoi@users.noreply.github.com>
2026-09-23 18:08:56 +0700

    Add files via upload

---
+++ submit_snakemake.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+module load Miniforge3/24.1.2-0
+eval "$(conda shell.bash hook)"
+conda activate lcwgs
+
+if [ "$#" -eq 0 ]; then
+    snakemake --forceall --rulegraph | dot -Tpdf > dag.pdf
+elif [ "$#" -eq 3 ]; then
+    master="$1"
+    cores="$2"
+    rule="$3"
+    snakemake -s pipelines/master_"$master".smk -c "$cores" --profile slurm/ "$rule"
+elif [ "$#" -eq 4 ]; then
+    master="$1"
+    cores="$2"
+    rule="$3"
+    option="$4"
+    snakemake -s pipelines/master_"$master".smk -c "$cores" "$option" --profile slurm/ --scheduler greedy "$rule"
+else
+    echo "Invalid inputs."
+    exit 1  # Exit with an error status
+fi
